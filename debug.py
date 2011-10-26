0e9934658142cb9258e4e0a4dd699dbf4fed9138
James R Eagan <eagan@telecom-paristech.fr>
2011-10-26 15:15:55 +0200

    Added licensinsing info (LGPL3).

--- debug.py
+++ debug.py
@@ -1,5 +1,20 @@
 # Misc debugging utility functions
-# Copyright (C) 2003-2009 James R. Eagan <eaganjr@acm.org>
+# JRElib -- Python utility library
+# Copyright 2003-2011, James R. Eagan (code at my last name dot me)
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# and GNU Lesser General Public License along with this program.  
+# If not, see <http://www.gnu.org/licenses/>.
 
 import code
 import sys
